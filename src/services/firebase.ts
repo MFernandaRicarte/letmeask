738123b8dd944753ea17d1d27aeff143d79f5eaa
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2024-03-19 18:05:19 -0300

    adicionando arquivos da aula 1

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey:"AIzaSyD3ECv_M5diE9NAWHcqlg1hZ4NF5swHe9Y",
+    authDomain:"letmeask-76d18.firebaseapp.com",
+    databaseURL:"https://letmeask-76d18-default-rtdb.firebaseio.com",
+    projectId:"letmeask-76d18",
+    storageBucket:"letmeask-76d18.appspot.com",
+    messagingSenderId:"919092055398",
+    appId:"1:919092055398:web:3b4c5130c5bb6aca7b8ca7"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
