a6a98727118022c1a4233c9e344697917bcca3d3
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2024-03-19 20:41:42 -0300

    alterações com cliques

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
@@ -15,8 +16,8 @@ export function NewRoom() {
             </aside>
             <main>
                 <div className= "main-content">
-                    <h2>Criar uma nova sala</h2>
-                    <img src={logoImg} alt="Letmeask" />
+                <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>       
                     <form>
                         <input
                         type='text' placeholder='Nome da sala'>
@@ -27,7 +28,7 @@ export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala existente? <a href="#">clique aqui </a>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui </Link>
                     </p>
                 </div>
             </main>
