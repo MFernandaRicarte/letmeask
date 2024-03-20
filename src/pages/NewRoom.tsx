c7348a9d7a7b0797ceea03a9bfd6e3a2a854efbf
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2024-03-20 09:00:28 -0300

    Redirecionamento de sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FormEvent, useContext, useState } from 'react';
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
@@ -9,7 +9,7 @@ import { database } from '../services/firebase';
 
 export function NewRoom() {
     const { user } = useAuth();
-
+    const history = useHistory();
     const [newRoom, setNewRoom] = useState('');
 
  async function handleCreateRoom(event: FormEvent) {
@@ -25,6 +25,8 @@ export function NewRoom() {
         title: newRoom,
         authorId: user?.id,
     })
+
+    history.push('/rooms/${firebaseRoom.key}')
  }
 
     return (
