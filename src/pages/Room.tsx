a970f6eea465fb13cab178842b681c6f0a7fb342
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2024-03-20 19:47:36 -0300

    Ouvindo novas perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -46,7 +46,7 @@ export function Room() {
     useEffect(() => {
         const roomRef = database.ref(`rooms/${roomId}`);
 
-        roomRef.once('value', room => {
+        roomRef.on('value', room => {
             const databaseRoom = room.val();
             const firebaseQuestions: FireBaseQuestions = databaseRoom.questions ?? {};
             const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value])=> {
