8a0702e72d89e1c3a1d7387a203ca52664c49be1
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2024-03-21 00:17:28 -0300

    Remoção de perguntas

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,5 +1,6 @@
 import { useParams} from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg'
+import deleteImg from '../assets/images/delete.svg'
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss'
@@ -8,6 +9,8 @@ import { FormEvent, useEffect, useState } from 'react';
 //import { database } from '../services/firebase';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
+
 
 type RoomParams = {
     id: string;
@@ -19,6 +22,12 @@ export function AdminRoom() {
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId);
+
+    async function handleDeleteQuestion(questionId: string) {
+       if (window.confirm('Tem certeza que deseja excluir esta pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+       }
+    }
 
     return (
         <div id = "page-room">
@@ -44,7 +53,14 @@ export function AdminRoom() {
                             key = {question.id}
                             content = {question.content}
                             author = {question.author}
-                        />
+                        >
+                            <button
+                                type = "button"
+                                onClick = {() => handleDeleteQuestion(question.id)}
+                            >
+                                <img src = {deleteImg} alt = "Remover pergunta" />
+                            </button>
+                        </Question>
                     );
                 })}
                 </div>
