fc9a829366226d3e0d0aba18d1dd685a53605807
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2024-03-21 00:03:11 -0300

    Funcionalidade de likes

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,8 +4,8 @@ import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss'
 import { FormEvent, useEffect, useState } from 'react';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
+//import { useAuth } from '../hooks/useAuth';
+//import { database } from '../services/firebase';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
 
@@ -14,38 +14,11 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-    const { user } = useAuth();
+//    const { user } = useAuth();
     const params = useParams<RoomParams>();
-    const [newQuestion, setNewQuestion] = useState('');
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId);
-
-    async function handleSendQuestion(event: FormEvent) {
-        event.preventDefault();
-
-        if (newQuestion.trim() == '') {
-            return;
-        }
-
-        if (!user) {
-            throw new Error('You must be logged in');
-        }
-
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user?.name,
-                avatar: user.avatar,
-            },
-            isHighlighted: false,
-            isAnswered: false
-        };
-
-        await database.ref(`rooms/${roomId}/questions`).push(question);
-
-        setNewQuestion('');
-    }
 
     return (
         <div id = "page-room">
